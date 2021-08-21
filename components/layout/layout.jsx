64317ede05293b9369364c5a4f6c03e47c4060f8
Pramod <bhusalpramod456@gmail.com>
2021-08-21 16:22:37 +0545

    added meta tags

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -1,9 +1,18 @@
 import Navigations from "../navigation/navigations";
 import { Fragment } from "react";
+import Head from 'next/head';
 
 export default function Layout({ children }) {
   return (
     <Fragment>
+      <Head>
+        <title>{`Pramod's Portfolio`}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charset="utf" />
+        <meta name="description" content="Pramod bhusal portfolio website. Engineering graduate from tribhuwan university Nepal"/>
+        <meta  name="keywords" content="Pramod , bhusal, portfolio, engineer, developer"/>
+        <meta name="author" content="Pramod bhusal"/>
+      </Head>
       <header>
         <Navigations />
       </header>
